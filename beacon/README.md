ab306ccab547585ab95dbfcd3de5756f723cfeca
byam <byambaa.mng@gmail.com>
2016-05-08 05:36:09 +0900

    defragment

--- beacon/README.md
+++ beacon/README.md
@@ -32,7 +32,7 @@ $ node receive.js 1
 ```sh
 # change 'accuracy' value to 0.1, if 'immediate'
 $ vi node_modules/bleacon/lib/bleacon.js
-else if (accuracy < 0.2) {
+else if (accuracy < 0.5) {
     proximity = 'immediate';
 }
 ```
--- beacon/receive.js
+++ beacon/receive.js
@@ -1,63 +1,86 @@
+// load modules
 var request = require('request');
+var Bleacon = require('bleacon');
+var groveSensor = require('jsupm_grove');
 
 
 // variables
+var uri_server = 'http://junction-test.mybluemix.net/api/beacon?';
 var uuid_list = ['485344424c45414480c01800ffffbbbb'];
 var checkpoint = process.argv[2];
 var flag = true;
-var d1 = new Date();
+var date1 = new Date();
+var track_interval = 1000;
 
 console.log("Your CHECKPOINT NUMBER is: " + checkpoint);
 
-// start tracking
-Bleacon = require('bleacon');
-Bleacon.startScanning();
 
-var f = function(bleacon) {
-    var d2 = new Date();
+// ==== LED ====
+var led = new groveSensor.GroveLed(2);
 
-    if (d2 - d1 > 1000){
-      d1 = d2;
-        // debug
-        if (uuid_list.indexOf(bleacon.uuid) != -1 && bleacon.proximity != 'immediate'){
-          console.dir(bleacon.proximity);
-        } else
-        {
-          console.log("NO SIGNAL");
-        }
-       // console.log('checkpoint: ' + checkpoint)
-       // console.log('uuid: ' + bleacon.uuid);
-       // console.log('distance: ' + bleacon.proximity);
+function ledLight(){
+  led.write(1);
+  setTimeout(function(){
+    led.write(0);
+  }, 3000);
+}
 
-        var options = {
-          uri: 'http://localhost:9090/?last_checkpoint_id=' + checkpoint + '&beacon_id=' + bleacon.uuid,
-          form: { name: 'テストユーザー' },
-          json: true
-        };
 
-        if (flag && bleacon.proximity == 'immediate' && uuid_list.indexOf(bleacon.uuid) != -1){
-            request.get(options, function(error, response, body){
-              if (error)
-              {
-                console.log("***GET REQUEST ERROR***");
-                console.log(bleacon.uuid);
-                console.log(bleacon.proximity);
-                console.log(checkpoint);
-              }
-              else if (response.statusCode == 200) {
-                console.log("***GET REQUEST SUCCESS***");
-                console.log(bleacon.uuid);
-                console.log(bleacon.proximity);
-                console.log(checkpoint);
-                // stop POST-ing
-                flag = false;
-              } else {
-                console.log('error: '+ response.statusCode);
-              }
-            });
-        }
+// ==== BEACON ====
+var beaconTrack = function(bleacon) {
+  var date2 = new Date();
+
+  // track interval
+  if (date2 - date1 > track_interval){
+    date1 = date2;
+
+    // Signal status & distance
+    if (uuid_list.indexOf(bleacon.uuid) != -1) {
+      if (bleacon.proximity != 'immediate') {
+        console.log(bleacon.proximity);
+      }
+    } else {
+      console.log("NO SIGNAL");
     }
-  };
+
+    // set uri
+    var options = {
+      uri: uri_server + 'last_checkpoint_id=' + checkpoint + '&beacon_id=' + bleacon.uuid
+    };
+
+    // beacon checked on Checkpoint
+    if (flag && bleacon.proximity == 'immediate' && uuid_list.indexOf(bleacon.uuid) != -1) {
+
+      // send GET REQUEST
+      request.get(options, function(error, response, body){
+
+        if (response.statusCode == 200) {
+          console.log("*** SUCCESS ***");
+          console.log(bleacon.uuid);
+          console.log(bleacon.proximity);
+          console.log(checkpoint);
+          ledLight();
+
+          // stop POST-ing
+          flag = false;
+        } else {
+          console.log("### ERROR ### ");
+          console.log('error: '+ response.statusCode);
+          console.log(bleacon.uuid);
+          console.log(bleacon.proximity);
+          console.log(checkpoint);
+          ledLight();
+        }
+      });
+    } else if (bleacon.proximity == 'immediate' && uuid_list.indexOf(bleacon.uuid) != -1) {
+      console.log(bleacon.proximity);
+      ledLight();
+    }
+  }
+};
 
 
-Bleacon.on('discover', f);
+
+// Start Tracking
+Bleacon.startScanning();
+Bleacon.on('discover', beaconTrack);
