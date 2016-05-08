ca99ad21d3b11b13ecb6142f35033f9de86facac
Amar Zanashir <amarzanashir+github@gmail.com>
2016-05-08 11:33:54 +0900

    Change database credentials

--- junction-app/settings.py
+++ junction-app/settings.py
@@ -7,13 +7,10 @@ DEBUG = True
 SECRET_KEY = 'junctionmgls' # secret key for app
 
 # mysql settings
-MYSQL_DATABASE_HOST = 'us-cdbr-iron-east-03.cleardb.net'
-MYSQL_DATABASE_PORT = 3306
-MYSQL_DATABASE_USER = 'b22d82b5e57d67'
-MYSQL_DATABASE_PASSWORD = 'cbccb1c0'
-MYSQL_DATABASE_DB = 'ad_bcdddb8fbd90b7a'
+MYSQL_DATABASE_HOST = <HOST>
+MYSQL_DATABASE_PORT = <PORT>
+MYSQL_DATABASE_USER = <USERNAME>
+MYSQL_DATABASE_PASSWORD = <PASSWORD>
+MYSQL_DATABASE_DB = <DATABASE>
 MYSQL_DATABASE_CHARSET = 'utf8'
-# format is dialect+driver://username:password@host:port/database
-SQLALCHEMY_DATABASE_URI = 'mysql://b22d82b5e57d67:cbccb1c0@us-cdbr-iron-east-03.cleardb.net:3306/ad_bcdddb8fbd90b7a'
-
-#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE
+SQLALCHEMY_DATABASE_URI = <URI>
